5862fa8dcb4d88cb3a006b5bfb46f763ca77396d
bgonzalez <benjamin.gonzalez@ayi.group>
2023-09-16 12:09:00 -0300

    ajustes sobre app-sin-express.js

--- app-sin-express.js
+++ app-sin-express.js
@@ -1,17 +1,12 @@
 const http = require('http');
 require('dotenv').config();
-
-
 const { PORT } = process.env;
 
 http.createServer(
     (req, res) => {
-
         // res.writeHead(200, { 'Content-Type': 'application/json' });
-
         res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
         res.writeHead(200, { 'Content-Type': 'application/csv' });
-        
         res.write('id ; Nombre\n');
         res.write('1 ; Benja\n');
         res.write('2 ; Benja2\n');
